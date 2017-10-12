454fab80c7035bb8df2b6ba6edcf9d02d9cc7516
SuperOranges <ac1234d5@126.com>
2017-10-12 14:03:37 +0800

    meeting
    meeting

--- CKPT/app/src/main/java/com/sun/ckpt/services/CKPTSystemServices.java
+++ CKPT/app/src/main/java/com/sun/ckpt/services/CKPTSystemServices.java
@@ -1,10 +1,15 @@
 package com.sun.ckpt.services;
 
+import com.sun.ckpt.bean.taskBean;
+import com.sun.ckpt.bean.userBean;
+
 /**
  * Created by Administrator on 2017/10/9.
  */
 
 public interface CKPTSystemServices {
-
+      void  requestData();//请求数据
+      taskBean receiveTaskData();//接收Task数据
+      userBean receiverUserData();//接收用户数据
 
 }
