e8519a15cef1dcae117c9b5950ef6993d7827ac2
SuperOranges <ac1234d5@126.com>
2017-12-27 18:30:14 +0800

    PD-29
    taskdetail

--- PD-MVP-Clean/app/src/main/java/com/sun/pd_mvp_clean/taskdetail/TaskDetailActivity.java
+++ PD-MVP-Clean/app/src/main/java/com/sun/pd_mvp_clean/taskdetail/TaskDetailActivity.java
@@ -1,29 +1,70 @@
 package com.sun.pd_mvp_clean.taskdetail;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 
 import com.sun.pd_mvp_clean.R;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2017/12/1.
  */
 
 public class TaskDetailActivity extends AppCompatActivity {
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.taskdetail_act);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
         ab.setDisplayShowHomeEnabled(true);
+
+        //Detail ListView
+        ListView mDetailLV = (ListView)findViewById(R.id.lv_taskdetail);
+
+
+
     }
 
+    private static class TaskDetailAdapter extends SimpleAdapter {
+
+        private HashMap<String,String> mTaskdetailMap;
+
+        public TaskDetailAdapter(Context context, List<? extends Map<String, ?>> data, int resource, String[] from, int[] to) {
+            super(context, data, resource, from, to);
+        }
+
+        @Override
+        public CharSequence[] getAutofillOptions() {
+            return new CharSequence[0];
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
 
 
 }
